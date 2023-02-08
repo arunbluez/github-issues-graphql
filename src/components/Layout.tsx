2a1f2a455cd6440ff27add6f9f47eaf12d0cc6d4
Arunkumar Sugadevan <arunbluez@gmail.com>
2023-02-08 14:34:12 +0100

    fixed minor css bug

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <main className="relative overflow-hidden bg-gray-100 dark:bg-gray-800">
-      <div className="flex w-full flex-col pl-0 md:space-y-4 md:p-4">
+      <div className="flex min-h-screen w-full flex-col pl-0 md:space-y-4 md:p-4">
         {children}
       </div>
     </main>
